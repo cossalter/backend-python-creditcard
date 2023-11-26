99ee17875140637357c6bed363d4a6ff7a93e8d5
cossalter <menegassehugo@gmail.com>
2023-11-26 18:35:56 -0300

    feat(card): endpoint to create credit card and parameters rules
    - exp_date:
    	- required
    	- string value is valid (mm/yyyy);
    	- is a future date;
    
    - holder:
    	- required
    	- must have more than 2 characters
    
    - number:
    	- required
    	- check if the value is valid with the [lib](https://github.com/MaisTodos/python-creditcard)
    
    - cvv:
    	- no required
    	- must have 3 or 4 character length
    	- is int type

--- app/routers/credit_card.py
+++ app/routers/credit_card.py
@@ -1,3 +1,24 @@
+from pydantic import BaseModel, Field
+
 from base.router import BaseAPIRouter
 
 router = BaseAPIRouter(prefix="/credit-card", tags=["credit-card"])
+
+# POST
+
+
+class CreateCardInput(BaseModel):
+    exp_date: str = Field(
+        title="Expiration data",
+        min_length=7,
+        max_length=7,
+        pattern="[0-1][0-9]/[0-9]{4}",
+    )
+    holder: str = Field(title="Name of card owner", min_length=2)
+    number: str = Field(title="Card number", min_length=16, max_length=16)
+    cvv: int | None = Field(title="Security code", ge=100, le=9999, default=None)
+
+
+@router.post("/")
+async def create(input: CreateCardInput):
+    return input
