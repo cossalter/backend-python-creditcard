db9e95966dfb9a4d9430426c2360f92ee11cb779
cossalter <menegassehugo@gmail.com>
2023-11-27 11:47:59 -0300

    feat(test): add user entity tests

---
+++ app/user/tests/test_entity.py
@@ -0,0 +1,26 @@
+from user.entity import User
+
+
+class TestUserEntity:
+    def test_create_user_entity(self):
+        user = User(
+            username="fulaninho",
+            password="teste123",
+            is_active=True,
+        )
+
+        assert user.username == "fulaninho"
+        assert user.password == "teste123"
+        assert user.is_active is True
+
+    def test_user_confirm_password(self):
+        user = User(
+            username="foobar",
+            password="teste123",
+            is_active=True,
+        )
+
+        assert user.confirm_password("teste123")
+
+        # Not the same password
+        assert not user.confirm_password("123456")
