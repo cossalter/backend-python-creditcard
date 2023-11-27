5c00587da54b8fca2d198039d1d75fdd8c02b930
cossalter <menegassehugo@gmail.com>
2023-11-27 02:13:40 -0300

    feat(user): add the 'me' endpoint

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,3 +1,12 @@
 from base.router import BaseAPIRouter
 
+from user.entity import User
+from routers.auth import CurrentUser
+
+
 router = BaseAPIRouter(prefix="/users", tags=["user"])
+
+
+@router.get("/me", response_model=User)
+async def me(current_user: CurrentUser):
+    return current_user
