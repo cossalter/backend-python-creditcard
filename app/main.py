49443fd32670b8632f325073fd446a23cf2851fa
cossalter <menegassehugo@gmail.com>
2023-11-27 02:05:23 -0300

    feat(env): load enviroment variables

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 import routers
+
+from dotenv import load_dotenv
 from fastapi import FastAPI
 
-app = FastAPI()
+
+class BaseFastAPI(FastAPI):
+    def setup(self) -> None:
+        load_dotenv()
+
+        super().setup()
+
+
+app = BaseFastAPI()
 
 for router in routers.all_router:
     app.include_router(router)
