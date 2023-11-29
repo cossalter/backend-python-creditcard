01d88b0278badcda90af0ebc7b5898835eb66bfb
cossalter <menegassehugo@gmail.com>
2023-11-29 18:23:55 -0300

    fix(init-package): add missing pagake initiation and mock lib

--- app/conftest.py
+++ app/conftest.py
@@ -1,5 +1,35 @@
-import os
-import sys
+import pytest
 
-# Some imports break when run the tests and don't do this
-sys.path.append(os.getcwd())
+from main import app
+
+from fastapi.testclient import TestClient
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from database.base import Base
+
+client = TestClient(app)
+
+
+# Create database session for tests
+@pytest.fixture
+def _create_db_session():
+    engine = create_engine(
+        "sqlite:///:memory:", connect_args={"check_same_thread": False}
+    )
+    session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+    Base.metadata.create_all(bind=engine)
+
+    return session
+
+
+@pytest.fixture
+def db_session(_create_db_session):
+    db = _create_db_session()
+
+    try:
+        yield db
+    finally:
+        db.close()
