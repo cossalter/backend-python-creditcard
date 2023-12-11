8c0a4561ab0237da0974af3f5a1689187c4690ce
cossalter <menegassehugo@gmail.com>
2023-12-11 10:56:08 -0300

    feat(card-tests): adding tests for credit card
    - check if the day in expiration date is the last day in month;
    - add test to create factory method and invalid string formats when
    use the create factory method;

--- app/entities/tests/test_credit_card.py
+++ app/entities/tests/test_credit_card.py
@@ -1,3 +1,8 @@
+"""
+All the fake data like number, exp_date and cvv to create the credit card tests,
+was generate by https://www.4devs.com.br/gerador_de_numero_cartao_credito
+"""
+
 import pytest
 import datetime
 from freezegun import freeze_time
@@ -31,6 +36,20 @@ class TestCreditCard:
 
         assert str(e.value) == "Expiration date must be a future date!"
 
+    @freeze_time("2023-12-11")
+    def test_not_month_last_day(self):
+        with pytest.raises(ValueError) as e:
+            CreditCard(
+                exp_date=datetime.date(2025, 10, 1),
+                holder="Fulano de Tal",
+                number="5400437724026876",
+            )
+
+        assert (
+            str(e.value)
+            == "The day in expiration date must be the last one in the month!"
+        )
+
     @freeze_time("2023-11-26")
     def test_invalid_number(self):
         with pytest.raises(ValueError) as e:
@@ -41,3 +60,42 @@ class TestCreditCard:
             )
 
         assert str(e.value) == "The card number isn't valid!"
+
+    @freeze_time("2023-12-11")
+    @pytest.mark.parametrize("exp_date", ["05/2025", datetime.date(2025, 5, 31)])
+    def test_create_factory(self, exp_date: str | datetime.date):
+        card = CreditCard.create(
+            exp_date=exp_date,
+            holder="Fulano de Tal",
+            number="5406183511065370",
+            cvv=851,
+        )
+
+        assert card.exp_date == datetime.date(2025, 5, 31)
+        assert card.holder == "Fulano de Tal"
+        assert card.number == "5406183511065370"
+        assert card.cvv == 851
+
+    @pytest.mark.parametrize(
+        "exp_date",
+        [
+            "11/05/2025",
+            "5/2025",
+            "2025/05",
+            "2025/5",
+            "2025/05/11",
+            "2025/11/05",
+        ],
+    )
+    def test_create_factory_with_wrong_str_date_format(self, exp_date: str):
+        with pytest.raises(ValueError) as e:
+            CreditCard.create(
+                exp_date=exp_date,
+                holder="Fulano de Tal",
+                number="5406183511065370",
+                cvv=851,
+            )
+
+        assert (
+            str(e.value) == "The date parameter must be in the following format %m/%Y"
+        )
