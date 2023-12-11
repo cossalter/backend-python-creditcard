40d69c7b96dd62dd74c1791201d52b16289ebd98
cossalter <menegassehugo@gmail.com>
2023-12-11 11:00:42 -0300

    feat(user-tests): add test for method add_card in user entity

--- app/entities/tests/test_user.py
+++ app/entities/tests/test_user.py
@@ -1,9 +1,38 @@
+import datetime
+
 from entities.user import User
+from entities.credit_card import CreditCard
+
+from freezegun import freeze_time
 
 
 class TestUserEntity:
-    def test_create_user_entity(self):
+    def test_create(self):
         user = User(username="fulaninho", is_active=True)
 
         assert user.username == "fulaninho"
         assert user.is_active is True
+
+    @freeze_time("2023-12-11")
+    def test_add_card(self):
+        user = User(username="fulaninho", is_active=True)
+
+        cards = [
+            CreditCard(
+                exp_date=datetime.date(2025, 10, 31),
+                holder="Fulano de Tal",
+                number="5400437724026876",
+                cvv=871,
+            ),
+            CreditCard(
+                exp_date=datetime.date(2024, 6, 30),
+                holder="Fulano de Tal",
+                number="5276334697446499",
+                cvv=537,
+            ),
+        ]
+
+        for card in cards:
+            user.add_card(card)
+
+        assert user.cards == cards
